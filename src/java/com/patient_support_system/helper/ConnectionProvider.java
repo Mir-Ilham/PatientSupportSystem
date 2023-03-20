6c6f3ea4c0299d162c5a360804e5920ad659bfc2
mir-ilham <mhmilham3@gmail.com>
2023-03-20 22:24:41 +0600

    Project structure setup

---
+++ src/java/com/patient_support_system/helper/ConnectionProvider.java
@@ -0,0 +1,22 @@
+package com.patient_support_system.helper;
+
+import java.sql.*;
+
+public class ConnectionProvider {
+
+    private static Connection con;
+
+    public static Connection getConnection() {
+        try {
+            if (con == null) {
+                // Load driver class
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                // Create a connection
+                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mint_blog", "ilham", "23498Devel$");
+            }
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return con;
+    }
+}
